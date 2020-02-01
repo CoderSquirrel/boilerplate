e66ca01b2d3e364101c90f3dd96d32034452abc0
Alan Balen Schio <alanbschio@gmail.com>
2020-02-01 16:32:51 -0300

    Adicionado seleçao de ganhador, icones novos e finalizacao de jogo

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,23 +1,45 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+  <main id="app">
+      <ct-menu></ct-menu>
+    <section>
+       <router-view></router-view>
+      
+    </section>
+    <footer>
+      Feito por Alan (@schirrel)
+      </footer>
+  </main>
+  
 </template>
 
 <script>
+import Menu from '@/components/Menu'
 export default {
-  name: 'App'
+  name: 'App',
+  components: {
+    'ct-menu': Menu
+  },
+  data () {
+    return {
+    }
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+main,section{
+  position: relative;
+  margin: 0px;
+}
+section {
   margin-top: 60px;
 }
 </style>
--- template/src/router/index.js
+++ template/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Players from '@/components/Players'
+import Game from '@/components/Game'
 
 Vue.use(Router)
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Game',
+      component: Game
+    },
+    {
+      path: '/jogadores',
+      name: 'Jogadores',
+      component: Players
     }
   ]
 })
